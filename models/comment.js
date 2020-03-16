dd1733ab0653484ccfb16b6c211c4327beba1a13
amara <amaramootaz11@gmail.com>
2020-03-16 20:38:43 +0100

    today update

--- models/comment.js
+++ models/comment.js
@@ -4,10 +4,12 @@ const commentSchema = new mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
     time:{type:Date,required:true},
     content:{type:String,required:true},
+   
     user:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
     project:{type:mongoose.Schema.Types.ObjectId,ref:'project'},
     job:{type:mongoose.Schema.Types.ObjectId,ref:'job'},
     company:{type:mongoose.Schema.Types.ObjectId,ref:'company'}
+
 });
 
 module.exports=mongoose.model('comment',commentSchema);
