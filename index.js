d0f657ef3c36507e708285bd24c27cc04c38c24c
amara <amaramootaz11@gmail.com>
2020-03-16 20:38:12 +0100

    today update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const jobRoute = require("./routes/job");
 const projectRoute = require("./routes/project");
 const companyRoute = require("./routes/company");
 const skillRoute = require("./routes/skill");
-
+const commentRoute = require("./routes/comment");
 
 const app=Express()
 
@@ -23,9 +23,11 @@ app.use(BodyParser.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
-const password ="rbTYm11n9UXBtAeE";
+const password ="4NdwfdVtGM84LID1";
 //mongodb://127.0.0.1:27017/linkedin
-const uri="mongodb://127.0.0.1:27017/linkedin";
+//const uri="mongodb://127.0.0.1:27017/linkedin";
+const uri="mongodb+srv://amara11:"+password+"@cluster0-09veh.mongodb.net/test?retryWrites=true&w=majority";
+
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false})
 
 mongoose.connection.on('error',()=>{console.log('connection failed')});
@@ -44,6 +46,7 @@ app.use('/friend',friendRoute);
 app.use('/company',companyRoute);
 app.use('/project',projectRoute);
 app.use('/job',jobRoute);
+app.use('/comment',commentRoute);
 
 
 
@@ -53,13 +56,13 @@ app.set('port',process.env.port || 3000);
 app.listen(app.get('port'),()=>{
     console.log(`server is running on port  ${app.get('port')}`)});
 
-//Model mte3 job wel project nahi field company w 5ali field barka user e5dem bih ynajim ye5ou iduser wela idcompany ma8ir reference al les deux models 5ater mahesh haja kbira msh lezem fel faza adhiya ne5dmou b populate
-//getlatestFeeds (ordered by time) jib beha e5er projects w jobs habtouhom les amis mte3 l company wela l user
-//latestfeeds ordered by time eli bch yet7atou fel profile mte3 l user wela company 
-//j'aime wel commentaire , w getLikesBypost w getCommentsByposts
-//Getfriendslist , getOnlinefriends
-//gettopjobs(_id,title,salary,description)
-//getTopprofiles (ordered by number of friendlist)
-//getsuggestions 
+
+
+
+
+
+
+
+
 //N.B: l5edma hethy lkol eli fel backend matbadil feha chey hatta les models matbadalsh fihom 
 
