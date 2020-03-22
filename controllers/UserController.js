69de271d19c7976a6f866be4a7ddffea3c3b2e8b
Your Name <you@example.com>
2020-03-22 18:05:06 +0100

    delete model company

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -326,9 +326,9 @@ exports.signup=function(req,res){
                     email:req.body.email,
                     category:req.body.category,
                     password:hashedPass,
-                    image:"uploads\1584895914321user-pro-img.png",
+                    image:"uploads\\1584895914321user-pro-img.png",
                     title:" ",
-                    cover:"uploads\1584439090144about.png"                 
+                    cover:"uploads\\1584439090144about.png"                 
                 });
 
                const transporter = nodemailer.createTransport({
