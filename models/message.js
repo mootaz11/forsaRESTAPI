9bbd6e46169b41360ebf46a884571e09df47a25c
amara <amaramootaz11@gmail.com>
2020-03-30 19:37:31 +0200

    message commit

--- models/message.js
+++ models/message.js
@@ -2,8 +2,8 @@ const mongoose=require("mongoose");
 messageSchema=new mongoose.Schema({
 _id:mongoose.Schema.Types.ObjectId,    
 text :String,
-sentBy:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
-sentTo:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
-date:Date
+sender:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
+receiver:{type:mongoose.Schema.Types.ObjectId,ref:'user'},
+date:String
 });
 module.exports=mongoose.model('message',messageSchema);
